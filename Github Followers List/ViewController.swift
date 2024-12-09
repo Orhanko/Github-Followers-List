61e1686dccda4758a245e1220ad33d1e60ea3364
Orhan <95143599+Orhanko@users.noreply.github.com>
2024-12-09 12:17:59 +0100

    Created Xcode project and deleted main storyboard file

---
+++ Github Followers List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Github Followers List
+//
+//  Created by Orhan Pojskic on 12/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Evo meee")
+        view.backgroundColor = .green
+    }
+
+
+}
+
