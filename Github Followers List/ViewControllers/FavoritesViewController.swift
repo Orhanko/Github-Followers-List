72ee4940d3bcea3cbd5cb27a04e46dd6f1bc71ce
Orhan <95143599+Orhanko@users.noreply.github.com>
2024-12-23 20:08:38 +0100

    Implemented tavorites array to a FavoritesViewController and implemented delte functionality

--- Github Followers List/ViewControllers/FavoritesViewController.swift
+++ Github Followers List/ViewControllers/FavoritesViewController.swift
@@ -8,18 +8,80 @@
 import UIKit
 
 class FavoritesViewController: UIViewController {
-
+    let tableView = UITableView()
+    var favorites: [Followers] = []
     override func viewDidLoad() {
         super.viewDidLoad()
-        //TODO: 9:48:43
-        PersistenceManager.retrieveFavorites { result in
+        configuretableView()
+        getFavorites()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        getFavorites()
+    }
+    
+    func configuretableView(){
+        view.addSubview(tableView)
+        tableView.frame = view.bounds
+        tableView.rowHeight = 80
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(FavoriteCell.self, forCellReuseIdentifier: FavoriteCell.reuseIdentifier)
+    }
+    
+    func getFavorites() {
+        PersistenceManager.retrieveFavorites { [weak self] result in
+            guard let self = self else {return}
             switch result {
             case .success(let favorites):
-                print(favorites)
+                self.favorites = favorites
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                    
+                }
             case .failure(let error):
-                print(error)
+                showAlert(title: "Error", message: error.rawValue)
             }
         }
     }
 
+}
+
+extension FavoritesViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        favorites.count
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let favorite = favorites[indexPath.row]
+        let destVC = FollowersListViewController()
+        destVC.username = favorite.login
+        destVC.title = favorite.login
+        
+        navigationController?.pushViewController(destVC, animated: true)
+        
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        guard editingStyle == .delete else { return }
+        
+        let favorite = favorites[indexPath.row]
+        favorites.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .left)
+        
+        PersistenceManager.updateWith(favorite: favorite, actionType: .remove) {[weak self] result in
+            guard let self else { return}
+            
+            guard let error = result else {return}
+            self.showAlert(title: "Error", message: "Unable to delete")
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: FavoriteCell.reuseIdentifier) as! FavoriteCell
+        let favorite = favorites[indexPath.row]
+        cell.set(favorite: favorite)
+        return cell
+    }
 }
